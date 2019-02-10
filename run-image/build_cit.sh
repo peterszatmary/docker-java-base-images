5297c62fda04c844fef5392e9e4651ce4255d644
Peter Szatmary <peter.szatmary@codexa.sk>
2019-02-10 19:14:37 +0100

    deleted unecessary comments.

--- run-image/build_cit.sh
+++ run-image/build_cit.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-# app         : any name
-# environment : dev / sit / cit / prod
 docker build  -t run_base_image:cit_pp_1.0v --build-arg environment=cit --build-arg app=pp  .
--- run-image/build_sit.sh
+++ run-image/build_sit.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-# app         : any name
-# environment : dev / sit / cit / prod
 docker build  -t run_base_image:sit_pp_1.0v --build-arg environment=sit --build-arg app=pp  .
